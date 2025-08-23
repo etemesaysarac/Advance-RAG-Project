ba866d4a12f4b0f722f7750c0d50d0b728415a21
Etem Sarac <esay35@gmail.com>
2025-08-23 18:51:21 +0300

    I separated the documents into pieces for editing purposes, vectorized them, and performed the recall process.

--- ingestion.py
+++ ingestion.py
@@ -3,6 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+from openai import embeddings
 
 load_dotenv()
 
@@ -14,6 +15,23 @@ urls = [
 
 docs = [WebBaseLoader(url).load() for url in urls]
 doct_list =[item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter().from_tiktoken_encoder(
+    chunk_size=200, chunk_overlap =50)
+
+siplits =text_splitter.split_documents(doct_list)
+
+vectorstore = Chroma().from_documents(
+    documents=siplits,
+    collection_name="rag-chroma",
+    embedding=OpenAIEmbeddings(),
+    persist_directory="./.chroma"
+)
+
+retriver = Chroma(
+    collection_name="rag-chroma",
+    persist_directory="./.chroma",
+    embedding_function=OpenAIEmbeddings(),).as_retriever()
 
 if __name__ == '__main__':
     print(doct_list)
