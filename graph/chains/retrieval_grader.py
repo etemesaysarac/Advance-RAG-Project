190f2416ed5c2e50eda532d3a89bef8319a9ad0a
Etem Sarac <esay35@gmail.com>
2025-08-23 19:48:57 +0300

    I designed it functionally so that we can get a "yes" or "no" answer.

--- graph/chains/retrieval_grader.py
+++ graph/chains/retrieval_grader.py
@@ -2,5 +2,20 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel, Field
 
+from graph.chains.router import structured_llm_router
+
 """I'll check the data coming from router.py. I'll return a response asking if it's true or false. 
 If it's true, I can move on to the next step, but if it's false, I'll go back to the previous step and ask docs to fix the error."""
+
+llm = ChatOpenAI(temperature=0)
+#I dont want GPT to be hallucinated!
+
+class GradeDocuments(BaseModel):
+    """Binary scrore for relevance check on retrieved documents"""
+
+    binary_score: str =Field(
+        description="Documents are relevant to the question, 'yes' or 'no'",
+    )
+
+structured_llm_grader = llm.with_structured_output(GradeDocuments)
+#I wrote nearly same code at router.py
