61ec73ee7c449afef5f383ceb3ca522ebe7d3baf
Etem Sarac <esay35@gmail.com>
2025-08-23 18:39:14 +0300

    urls added from github.io docs designed. doc list created.

--- ingestion.py
+++ ingestion.py
@@ -1,6 +1,19 @@
 from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
 
 load_dotenv()
 
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+doct_list =[item for sublist in docs for item in sublist]
+
 if __name__ == '__main__':
-    print('PROJECT STARTED!!!!')
+    print(' ')
