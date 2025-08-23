de172c2b5190524b981ce6cca393a5557138ba6f
Etem Sarac <esay35@gmail.com>
2025-08-23 19:51:12 +0300

    system prompt wroted

--- graph/chains/retrieval_grader.py
+++ graph/chains/retrieval_grader.py
@@ -19,3 +19,7 @@ class GradeDocuments(BaseModel):
 
 structured_llm_grader = llm.with_structured_output(GradeDocuments)
 #I wrote nearly same code at router.py
+
+system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
+    If the document contains keyword(s) or semantic meaning related to the question, grade it as relevant. \n
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
