328bef16319a6db74b0cba00c2567a906e4b2679
Etem Sarac <esay35@gmail.com>
2025-08-25 15:37:03 +0300

    updated wit dotenv

--- graph/chains/generation.py
+++ graph/chains/generation.py
@@ -2,6 +2,9 @@ from click import prompt
 from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
 
 llm = ChatOpenAI(temperature=0)
 
