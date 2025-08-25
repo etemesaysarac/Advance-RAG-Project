c2919d4e82bf9e9c54d8b0bc512e83ab2bfc1b22
Etem Sarac <esay35@gmail.com>
2025-08-25 14:05:42 +0300

    def created.

--- graph/nodes/generate.py
+++ graph/nodes/generate.py
@@ -2,4 +2,6 @@ from graph.chains.generation import generation_chain
 #I imported the chain we created in generation.py into the generate.py file to use it.
 
 def generate ():
-    generation = generation_chain.invoke()
+    generation = generation_chain.invoke(
+        {"context": "documents", "question": "question"}
+    )
