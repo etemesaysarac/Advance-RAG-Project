9a137192404c0ba349711d716cde624b185f1650
Etem Sarac <esay35@gmail.com>
2025-08-25 13:08:36 +0300

    class created.

--- graph/hallucination_grader.py
+++ graph/hallucination_grader.py
@@ -3,3 +3,10 @@ from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel, Field
 
 llm = ChatOpenAI(temperature=0)
+
+class GradeHallucinations(BaseModel):
+    """Binary score for hallucination present in generated answer."""
+
+    binary_score: str =Field(
+        description="TAnswer is gounded in the facts, 'yes' or 'no'",
+    )
