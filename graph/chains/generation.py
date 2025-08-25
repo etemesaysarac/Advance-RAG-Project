8e2f3a43f6952da0fad268ffc754ad18f5af71dc
Etem Sarac <esay35@gmail.com>
2025-08-25 13:03:13 +0300

    generation_chain created

--- graph/chains/generation.py
+++ graph/chains/generation.py
@@ -6,3 +6,5 @@ from langchain_core.output_parsers import StrOutputParser
 llm = ChatOpenAI(temperature=0)
 
 prompt = hub.pull("rlm/rag-prompt")
+
+generation_chain = prompt | llm | StrOutputParser(()
