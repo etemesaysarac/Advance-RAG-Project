ec7fe345437814755db87c4fbc1678fe847973f0
Etem Sarac <esay35@gmail.com>
2025-08-25 14:14:28 +0300

    new libs added.state:GraphState used and added states. I returned and saved the user and system outputs to save them and use them in nodes.

--- graph/nodes/generate.py
+++ graph/nodes/generate.py
@@ -1,7 +1,18 @@
+from typing import Dict, Any
 from graph.chains.generation import generation_chain
+from graph.state import GraphState
+
+
 #I imported the chain we created in generation.py into the generate.py file to use it.
 
-def generate ():
+def generate (state : GraphState) -> Dict[str, Any]:
+    print("----------GENERATE----------")
+    question = state["question"]
+    documents = state["documents"]
+
     generation = generation_chain.invoke(
         {"context": "documents", "question": "question"}
     )
+
+    return {"generation": generation, "question": question, "documents": documents}
+# I returned and saved the user and system outputs to save them and use them in nodes.
--- graph/nodes/retrieve.py
+++ graph/nodes/retrieve.py
@@ -1,3 +1,12 @@
+from typing import Any, Dict
+from graph.state import GraphState
 from ingestion import retriver
 
-def retrieve():
+def retrieve(state : GraphState) -> Dict[str, Any]:
+    print("----------RETRIVE----------")
+
+    question = state["question"]
+    documents = retriver.invoke(question)
+
+    return {"question": question, "documents": documents}
+# I returned and saved the user and system outputs to save them and use them in nodes.
