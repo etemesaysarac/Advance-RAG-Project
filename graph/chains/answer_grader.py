f1145ada09b87bdc22b75065102f8d9771a51b01
Etem Sarac <esay35@gmail.com>
2025-08-25 13:20:42 +0300

    class created (this is nearly same with hallucination_grader.py)

--- graph/chains/answer_grader.py
+++ graph/chains/answer_grader.py
@@ -2,4 +2,11 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel, Field
 
+
+class GraderAnswer(BaseModel):
+
+    binary_score: str Field(
+        description="Answer addresses the question, 'yes' or 'no'"
+    )
+
 llm = ChatOpenAI(temperature=0)
