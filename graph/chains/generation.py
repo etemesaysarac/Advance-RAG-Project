6a0662d7016dff00f7718d8144df6a46507296d3
Etem Sarac <esay35@gmail.com>
2025-08-25 13:00:30 +0300

    Chatgpt accuracy share was written in the highest way.

--- graph/chains/generation.py
+++ graph/chains/generation.py
@@ -2,3 +2,5 @@ from click import prompt
 from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatOpenAI(temperature=0)
