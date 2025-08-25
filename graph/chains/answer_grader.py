b7f6a61b03a42ea10c9a086b28100088dc906f87
Etem Sarac <esay35@gmail.com>
2025-08-25 15:57:12 +0300

    updated

--- graph/chains/answer_grader.py
+++ graph/chains/answer_grader.py
@@ -12,7 +12,7 @@ class GraderAnswer(BaseModel):
     )
 
 llm = ChatOpenAI(temperature=0)
-structred_llm_grader = llm.structred_llm_grader(GraderAnswer)
+structred_llm_grader = llm.with_structured_output(GraderAnswer)
 
 system_prompt = """
 You are a grader assessing wheter an answer addresses / resolves a question. \n
@@ -21,10 +21,10 @@ Give a binary score 'yes' or 'no'. 'Yes' means that answer resolves the question
 
 answer_prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
-    ("human", "User question: \n\n {question}\n\n LLM generation: {generation")
+    ("human", "User question: \n\n {question}\n\n LLM generation: {generation}")
 ])
 
-answer_grader: RunnableSequence = answer_prompt | structured_llm_grader
+answer_grader : RunnableSequence = answer_prompt | structured_llm_grader
 
 """
 In answer_prompt, the question (the actual question asked by the user)-generation (LLM output) relationship was checked.
