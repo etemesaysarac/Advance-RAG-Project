d1e4ca59c807fd9ac9ad006cf578536f99cd762b
Etem Sarac <esay35@gmail.com>
2025-08-25 13:59:03 +0300

    I imported the chain we created in generation.py into the generate.py file to use it.

--- graph/nodes/generate.py
+++ graph/nodes/generate.py
@@ -1,4 +1,5 @@
 from graph.chains.generation import generation_chain
+#I imported the chain we created in generation.py into the generate.py file to use it.
 
 def generate ():
     generation = generation_chain.invoke()
