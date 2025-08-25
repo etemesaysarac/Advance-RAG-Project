38256608450e8e107ae27b42571aa80aae94ea50
Etem Sarac <esay35@gmail.com>
2025-08-25 15:47:51 +0300

    updated

--- graph/chains/hallucination_grader.py
+++ graph/chains/hallucination_grader.py
@@ -16,14 +16,14 @@ class GradeHallucinations(BaseModel):
 
 structured_llm_grader = llm.with_structured_output(GradeHallucinations)
 
-    system_prompt = """You are a grader assessing whether an LLM generation is grounded in / supported by a set of retrieved facts. \n 
+system_prompt = """You are a grader assessing whether an LLM generation is grounded in / supported by a set of retrieved facts. \n 
          Give a binary score 'yes' or 'no'. 'Yes' means that the answer is grounded in / supported by the set of facts."""
 
-    hallucination_prompt = ChatPromptTemplate.from_messages(
+hallucination_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", system_prompt),
             ("human", "Set of facts: \n\n {documents} \n\n LLM generation: {generation}"),
         ]
     )
 
-    hallucination_grader = hallucination_prompt | structured_llm_grader
+hallucination_grader = hallucination_prompt | structured_llm_grader
