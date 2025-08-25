c96733670ce0bf06afe77d99db9a25ce3b1436c3
Etem Sarac <esay35@gmail.com>
2025-08-25 14:59:00 +0300

    updaded

---
+++ graph/node_constants.py
@@ -0,0 +1,11 @@
+RETRIEVE = "retrieve"
+GRADE_DOCUMENTS = "grade_documents"
+GENERATE = "generate"
+WEBSEARCH = "websearch"
+
+
+
+"""
+When creating the general structure, I'll display the expressions in capital letters. 
+I'll make the necessary synchronizations within this file to avoid confusion.
+"""
