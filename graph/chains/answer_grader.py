df873f97472a83463cde0571b141e1d56b33606e
Etem Sarac <esay35@gmail.com>
2025-08-25 13:24:03 +0300

    system_prompt created.

--- graph/chains/answer_grader.py
+++ graph/chains/answer_grader.py
@@ -2,6 +2,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel, Field
 
+from graph.chains.retrieval_grader import system_prompt
+
 
 class GraderAnswer(BaseModel):
 
@@ -11,3 +13,8 @@ class GraderAnswer(BaseModel):
 
 llm = ChatOpenAI(temperature=0)
 structred_llm_grader = llm.structred_llm_grader(GraderAnswer)
+
+system_prompt = """
+You are a grader assessing wheter an answer addresses / resolves a question. \n
+Give a binary score 'yes' or 'no'. 'Yes' means that answer resolves the question. 
+"""
