a2a1a5d454306a7b5a41e3a6fb8488f98dda6889
Etem Sarac <esay35@gmail.com>
2025-08-25 15:08:55 +0300

    dotenv added. workflow created

--- graph/graph.py
+++ graph/graph.py
@@ -4,4 +4,11 @@ from graph.chains.router import question_router, RouteQuery
 from graph.state import GraphState
 from graph.chains.hallucination_grader import hallucination_grader
 from graph.chains.answer_grader import answer_grader
+from langgraph.graph import END, StateGraph
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+workflow = StateGraph(GraphState)
 
