f2fb83649ac4828423246b530f7c34727da64d47
Etem Sarac <esay35@gmail.com>
2025-08-25 15:04:16 +0300

    graph.chains.hallucination_grader file added here as a lib.

--- graph/graph.py
+++ graph/graph.py
@@ -2,4 +2,6 @@ from graph.node_constants import RETRIEVE, GENERATE, WEBSEARCH, GRADE_DOCUMENTS
 from graph.nodes import generate, grade_documents, web_search, retrieve
 from graph.chains.router import question_router, RouteQuery
 from graph.state import GraphState
+from graph.chains.hallucination_grader import hallucination_grader
+
 
